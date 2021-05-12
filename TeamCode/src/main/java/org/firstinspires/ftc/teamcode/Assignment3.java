a77b556eb579986ac9f6b637c99ad8abd71ef461
jrubacha <jrubacha@lsc.k12.in.us>
2021-05-12 07:22:34 -0400

    updates to assignment 3 file - gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assignment3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assignment3.java
@@ -1,71 +1,109 @@
-/** TODO: Read the following instructions
+// TODO: Read the following instructions
+/**
+ * The purpose of this assignment is to introduce you to using input in order to control functions.
  *
- *
+ * You will learn about logic statements and the gamepad.
  *
  * Except where prompted, do not make changes to the program.
  */
-
 //////////////////////////////////////////////////////////////////////////////////////////
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 //////////////////////////////////////////////////////////////////////////////////////////
-@Autonomous(name= "Assignment 3", group="Assignments")
-@Disabled
+@TeleOp(name="Assignment 3", group="actuators")
+//@Disabled        // Comment/Uncomment this line as needed to show/hide this opmode
 //////////////////////////////////////////////////////////////////////////////////////////
-public class Assignment3 extends LinearOpMode {
-    Actuators actuators;
-    Sensors sensors;
+
+public class Assignment3 extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
+    Sensors sensors;
+    Actuators actuators;
 
-    @Override
-    public void runOpMode() {
-        actuators = new Actuators(hardwareMap, telemetry);
-        sensors = new Sensors(hardwareMap, telemetry);
 
-        telemetry.addLine("Test board initialized and ready");
-        telemetry.update();
-
-        // TODO: Add code to make the servo go to position -1
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-        // Motion has started
-        runtime.reset();
-
-        while (opModeIsActive()) {
-            // TODO: Add code to make first motor spin forward at a power of 1
-
-            // TODO: Add code to make the servo go to position 1
-
-            // TODO: Run a method to make both motors spin forward at a power of 1. (When you run this code, the single motor code above should be commented out. This method will be written in actuators
-
-            updateTelemetry();
-        }
-
-    }
 //////////////////////////////////////////////////////////////////////////////////////////
-    private void runEverything(){
-        double motorPower = 0.25, servoPosition = 1;
-        int roundedRunTime = (int) runtime.seconds();
-        if (roundedRunTime % 5 == 0) {
-            motorPower = motorPower * -1;
-            servoPosition = servoPosition * -1;
+
+    /* Code to run ONCE when the driver hits INIT */
+    @Override
+    public void init() {
+        sensors = new Sensors(hardwareMap, telemetry);
+        actuators = new Actuators(hardwareMap, telemetry);
+
+        // Set up our telemetry dashboard
+        updateTelemetry(telemetry);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized    :)");
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY */
+    @Override
+    public void init_loop() {
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run ONCE when the driver hits PLAY */
+    @Override
+    public void start() {
+        runtime.reset();
+        updateTelemetry(telemetry);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP */
+    @Override
+    public void loop() {
+
+        // Call Telemetry
+        updateTelemetry(telemetry);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run ONCE after the driver hits STOP */
+    @Override
+    public void stop() {
+        telemetry.addData("Robot Stopped. ", "Have a nice day.");
+        telemetry.addData("Final runtime: ", runtime.toString());
+        telemetry.update();
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    public void checkDriverController() {
+        // Example if statement that adds a telemetry message if the square is pushed
+        if (gamepad1.square) {
+            telemetry.addLine("Square pushed");
         }
-        actuators.setServoPosition(servoPosition);
-        actuators.setMotorPower(motorPower);
-        actuators.setCRServoPower(motorPower);
+
+        // TODO: Use an if statement to add telemetry statement for the gamepad to show only when the triangle is pushed
+
+
+        // TODO: Complete the if-elseif-else statement below to control the servo position using the triangle and cross buttons. You will need to uncomment the code first
+        // You can quickly comment/uncomment code by highlighting all of it and press CTRL+/ on your keyboard
+//        if (gamepad1.) {
+//            actuators.setServoPosition(.5);
+//        } else if () {
+//
+//        }
+
+
+
+        // TODO: Set power to the motor based on the left_stick_y value
+        // Start typing actuators. (like above) to find motor methods
+
+
+
+
     }
 
-    private void updateTelemetry() {
-        // Show the elapsed game time
-        telemetry.addData("Run Time: ", runtime.toString());
-
-        // Display Values of all the attached sensors
-        sensors.printSensorTelemetry();
-    }
 }
+
