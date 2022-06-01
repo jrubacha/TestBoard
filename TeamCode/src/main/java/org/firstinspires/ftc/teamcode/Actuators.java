dfa38d7bcae1f5a596001fef0a1cb0ea2ca3fe9e
Jill_Rubacha <jrubacha@lsc.k12.in.us>
2022-06-01 09:23:42 -0400

    updated file to work with constants updated naming of components to match activity guide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuators.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuators.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import android.graphics.Color;
 import android.text.method.Touch;
 
@@ -8,24 +9,29 @@ import com.qualcomm.robotcore.hardware.AnalogSensor;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Actuators {
     private Servo myServo;
     private CRServo myCRServo;
     private DcMotor myMotor;
+    Constants constants;
     Telemetry telemetry;
 
     public Actuators(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
+        constants = new Constants();
         myServo = hardwareMap.get(Servo.class, "myServo");
-        myCRServo = hardwareMap.get(CRServo.class, "vexCRServo");
+        myCRServo = hardwareMap.get(CRServo.class, "myCRServo");
         myMotor = hardwareMap.get(DcMotor.class, "testBoardMotor");
+        myMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
 
@@ -41,6 +47,47 @@ public class Actuators {
     }
 
 
+
+    // Motor Modes
+    public void setMotorToEncoderMode(){
+        myMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void setMotorToBlankMode(){
+        myMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    // must be called AFTER setting a target position
+    public void setMotorToPositionMode(){
+        myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void setMotorTarget(int target){
+        myMotor.setTargetPosition(target);
+    }
+    public void resetEncoders(){
+        myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void printCurrentMotorMode(){
+        telemetry.addData("Motor Mode", myMotor.getMode());
+    }
+    public boolean isMotorBusy(){
+        return myMotor.isBusy();
+    }
+
+    public void driveToDistance (double inches){
+        myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        int target = (int)(inches / .046);
+        myMotor.setTargetPosition (target); //This converts inches back into pulses. 1 pulse covers about .046 inches of distance.
+        setMotorPower(.35);
+        myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (myMotor.isBusy())
+        {
+            telemetry.addData("Current Encoder Position", myMotor.getCurrentPosition() );
+            telemetry.addData("Target is", target);
+            telemetry.update();
+        }
+        setMotorPower(0);
+    }
 
     // Standard Servo
     public void setServoPosition(double pos) {
