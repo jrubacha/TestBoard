fc830ffe11dd08a8011f02a93f14890b3eaa1832
Jill_Rubacha <jrubacha@lsc.k12.in.us>
2022-06-01 09:22:49 -0400

    Added constants file to make my life easier

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    // Vex 393 Limits
+    public final double VEX_393_LOWER_BOUND = 0.15;
+    public final double VEX_393_UPPER_BOUND = 0.8;
+
+    // GoBilda Linear Servo Limits
+    public final double LINEAR_SERVO_LOWER_BOUND = 0.1;
+    public final double LINEAR_SERVO_UPPER_BOUND = 0.9;
+
+    // Driving Constants
+    public final double TURN_SPEED = 0.75;
+    public final double DRIVE_SPEED = 0.9;
+
+    // Encoder Targets
+    public final int TARGET_1 = 14000; // testing value. I made this up. It has no relevance
+
+}
