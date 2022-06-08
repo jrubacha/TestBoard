6daf384f3d2372e130e207e183641c26efa473f4
Jill_Rubacha <jrubacha@lsc.k12.in.us>
2022-06-07 22:57:10 -0400

    removal of code for template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assignment2a.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assignment2a.java
@@ -2,13 +2,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 //////////////////////////////////////////////////////////////////////////////////////////
 @Autonomous(name= "Assignment 2", group="Assignments")
 // TODO #1: Enable the Program
-//@Disabled
+@Disabled
 //////////////////////////////////////////////////////////////////////////////////////////
 public class Assignment2a extends LinearOpMode {
     BotUtilities util;
@@ -39,28 +40,13 @@ public class Assignment2a extends LinearOpMode {
 
     private void runEverything() {
         // TODO #2: Using variables - pot to servo
-        double potValue = sensors.getPotReading();
-        actuators.setServoPosition(potValue);
 
         // TODO #3: Using variables - pot to CR servo
-        actuators.setCRServoPower(potValue);
 
         // TODO #4: Telemetry by buttons
-        if (sensors.getLimitSwitchState()) {
-            telemetry.addLine("You pushed the button!");
-        } else {
-            telemetry.addLine("Hello darkness, my old friend");
-        }
-
 
         // TODO #5: Using variables - pot to Motor & button direction control
-        if (sensors.getPushButtonState()) {
-            actuators.setMotorPower(potValue);
-        } else if (sensors.getLimitSwitchState()) {
-            actuators.setMotorPower(-potValue);
-        } else {
-            actuators.setMotorPower(0);
-        }
+
     }
     private void callTelemetry() {
         // Show the elapsed game time
