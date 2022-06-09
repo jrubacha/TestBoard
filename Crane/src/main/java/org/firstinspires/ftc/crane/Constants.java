f988d6a6f1ae7137f300cbbb907040bca7de4c63
Jill_Rubacha <jrubacha@lsc.k12.in.us>
2022-06-09 14:06:55 -0400

    added constants for using victor spxs under pwm control

--- Crane/src/main/java/org/firstinspires/ftc/crane/Constants.java
+++ Crane/src/main/java/org/firstinspires/ftc/crane/Constants.java
@@ -1,9 +1,13 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.crane;
 
 public class Constants {
     // Vex 393 Limits
     public final double VEX_393_LOWER_BOUND = -0.8;
     public final double VEX_393_UPPER_BOUND = 0.8;
+
+    // Victor SPX Max/Min output
+    public final double VICTOR_SPX_LOWER_BOUND = -0.85;
+    public final double VICTOR_SPX_UPPER_BOUND = 0.85;
 
     // GoBilda Linear Servo Limits
     public final double LINEAR_SERVO_LOWER_BOUND = 0.1;
@@ -13,7 +17,5 @@ public class Constants {
     public final double TURN_SPEED = 0.75;
     public final double DRIVE_SPEED = 0.9;
 
-    // Encoder Targets
-    public final int TARGET_1 = 14000; // testing value. I made this up. It has no relevance
 
 }
