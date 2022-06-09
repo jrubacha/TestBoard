a87546a8a735707d049290a32eb012330992c3d6
Jill_Rubacha <jrubacha@lsc.k12.in.us>
2022-06-09 14:15:10 -0400

    added important details to comments

--- Crane/src/main/java/org/firstinspires/ftc/crane/Intake.java
+++ Crane/src/main/java/org/firstinspires/ftc/crane/Intake.java
@@ -4,5 +4,6 @@ public class Intake {
     /**
      * The intaking motor is 1 bag motor, with a ? : 1 reduction VP
      * The deploying motors are 2 bosch seat motors, electrically tied together
+     * This deploying motors really should have encoder control and/or limit switches at the outer bounds
      */
 }
--- Crane/src/main/java/org/firstinspires/ftc/crane/Turret.java
+++ Crane/src/main/java/org/firstinspires/ftc/crane/Turret.java
@@ -3,5 +3,6 @@ package org.firstinspires.ftc.crane;
 public class Turret {
     /**
      * The turret uses 1 Bag motor on a ? : 1 reduction versaplanetary
+     * This motor really should have encoder control and/or limit switches at the outer bounds
      */
 }
